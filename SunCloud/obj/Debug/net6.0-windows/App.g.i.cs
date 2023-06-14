0e4fce63fa348ae65c4b23cdfd1e637b51f1f439
reddosakura <alexander05511@gmail.com>
2023-06-14 14:30:01 +0300

    я апишка

--- SunCloud/obj/Debug/net6.0-windows/App.g.i.cs
+++ SunCloud/obj/Debug/net6.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace SunCloud {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace SunCloud {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.4.0")]
         public static void Main() {
             SunCloud.App app = new SunCloud.App();
             app.InitializeComponent();
